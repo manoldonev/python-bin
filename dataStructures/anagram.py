b75f847ed63a05943556f6a5bf0597c9cfeecfc4
Manol Donev <manol.donev@gmail.com>
2017-02-27 23:36:24 +0200

    Add anagram detection via prime product calculation

--- dataStructures/anagram.py
+++ dataStructures/anagram.py
@@ -4,13 +4,33 @@
 from collections import Counter
 
 
+def anagram_prime(s1, s2):
+    """Write a method to decide if two strings are anagrams or not."""
+
+    # O(n) time, O(1) space
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+              47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+
+    product1 = 1
+    for char1 in s1:
+        product1 *= primes[ord(char1) - 97]
+
+    product2 = 1
+    for char2 in s2:
+        product2 *= primes[ord(char2) - 97]
+
+    return product1 == product2
+
+
 def anagram_counter(s1, s2):
     """Write a method to decide if two strings are anagrams or not."""
+
     # O(n) time, O(n) space
     return Counter(s1) == Counter(s2)
 
 
 def anagram_sorted(s1, s2):
     """Write a method to decide if two strings are anagrams or not."""
+
     # O(nlogn) time, O(n) space
     return sorted(s1) == sorted(s2)
--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -1,37 +1,54 @@
 
 """Test Cases"""
 
+from dataStructures.anagram import anagram_prime
 from dataStructures.anagram import anagram_sorted
 from dataStructures.anagram import anagram_counter
 
 
+def test_anagram_prime():
+    assert anagram_prime("abcde", "bcdae")
+
+
+def test_anagram_prime2():
+    assert anagram_prime("", "")
+
+
+def test_not_anagram_prime3():
+    assert anagram_prime("abcde", "bcdaf") is False
+
+
+def test_not_anagram_prime4():
+    assert anagram_prime("abcde", "") is False
+
+
 def test_anagram_sorted():
-    assert anagram_sorted("abcde", "bcdae") == True
+    assert anagram_sorted("abcde", "bcdae")
 
 
 def test_anagram_sorted2():
-    assert anagram_sorted("", "") == True
+    assert anagram_sorted("", "")
 
 
 def test_not_anagram_sorted3():
-    assert anagram_sorted("abcde", "bcdaf") == False
+    assert anagram_sorted("abcde", "bcdaf") is False
 
 
 def test_not_anagram_sorted4():
-    assert anagram_sorted("abcde", "") == False
+    assert anagram_sorted("abcde", "") is False
 
 
 def test_anagram_counter():
-    assert anagram_counter("abcde", "bcdae") == True
+    assert anagram_counter("abcde", "bcdae")
 
 
 def test_anagram_counter2():
-    assert anagram_counter("", "") == True
+    assert anagram_counter("", "")
 
 
 def test_not_anagram_counter3():
-    assert anagram_counter("abcde", "bcdaf") == False
+    assert anagram_counter("abcde", "bcdaf") is False
 
 
 def test_not_anagram_counter4():
-    assert anagram_counter("abcde", "") == False
+    assert anagram_counter("abcde", "") is False
